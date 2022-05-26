7de4902c2c7e5c7b628627dfc62a1f82861a285b
chisimnulia <chisimokoye@gmail.com>
2022-05-26 20:47:59 +0100

    update

---
+++ movies_db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Movies_db (
+genre VARCHAR ,
+title VARCHAR,
+director VARCHAR,
+release_year INT,
+certificate TEXT,
+runtime TEXT,
+listed_in VARCHAR,
+overview TEXT,
+IMDB_Rating TEXT,
+Meta_score TEXT,
+No_of_Votes TEXT,
+Gross TEXT
+);
+
+
+SELECT * FROM Movies_db;
