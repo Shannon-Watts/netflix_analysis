b8af81523c94262f593ce6d317251af13b9f5a79
Chisimnulia Okoye <99673859+Chisimokoye@users.noreply.github.com>
2022-05-27 11:03:30 +0100

    Add files via upload
    table creation file

---
+++ movies_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Movies_db (
+genre VARCHAR ,
+title VARCHAR,
+director VARCHAR,
+release_year INT,
+runtime TEXT,
+listed_in VARCHAR,
+overview TEXT,
+IMDB_Rating TEXT,
+Meta_score TEXT,
+No_of_Votes TEXT,
+Gross TEXT
+);
+
+
+SELECT * FROM Movies_db;
